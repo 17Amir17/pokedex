5d691d251a2849ed3c19d84f5ed7303faa0790e8
17Amir17 <amirangel101@hotmail.com>
2021-10-15 17:32:55 +0300

    Api integration to get pokemon types

--- src/listeners/poketypeClick.js
+++ src/listeners/poketypeClick.js
@@ -1,6 +1,18 @@
-export function onPoketypeClick(event) {
+import { getPokemonsOfType } from "../networking/pokemonApi.js";
+export async function onPoketypeClick(event) {
   if (event.target.classList.contains("type")) {
     const type = event.target.dataset.type;
-    console.log(type);
+    try {
+      const response = await getPokemonsOfType(type);
+      const pokemon = response.data.pokemon;
+      const pokemonNames = [];
+      pokemon.forEach((pkmon) => {
+        console.log(pkmon);
+        pokemonNames.unshift(pkmon.pokemon.name);
+      });
+      console.log(pokemonNames);
+    } catch (error) {
+      alert(error);
+    }
   }
 }
--- src/networking/pokemonApi.js
+++ src/networking/pokemonApi.js
@@ -1,8 +1,19 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const baseURL = "https://pokeapi.co/api/v2/";
+const pokemonHeader = "pokemon/";
+const typeHeader = "type/";
 export async function getPokeomByNameOrId(pokemon) {
   try {
     if (!pokemon) throw "Search Query Empty";
-    const response = await axios.get(baseURL + pokemon);
+    const response = await axios.get(baseURL + pokemonHeader + pokemon);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getPokemonsOfType(type) {
+  try {
+    const response = await axios.get(baseURL + typeHeader + type);
     return response;
   } catch (error) {
     throw error;
